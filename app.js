fecc10dd2ab8f3af841e6a6941a18e587ea88596
anpu0708 <80457571+anpu0708@users.noreply.github.com>
2021-08-24 16:29:18 +0800

    finished app
    8daysaweek

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.json())  
+
 // set up author routes
 const authorRouter = require('./routes/authorRouter')
-// GET home page
+
+// handler for GET home page
 app.get('/', (req, res) => {
- res.send('<h1>CRM System</h1>')
+    res.send('<h1>Library System</h1>')
 })
-// Handle author-management requests
-// the author routes are added onto the end of '/author-management'
+
+// handler for author-management requests
+// author routes are added onto the end of '/author-management'
 app.use('/author-management', authorRouter)
+
 app.listen(3000, () => {
- console.log('The library app is listening on port 3000!')
+    console.log('The library app is listening on port 3000!')
 })
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,23 +1,38 @@
 //link to author model
 const authors = require('../models/author')
+
 // handle request to get all authors
 const getAllAuthors = (req, res) => {
- res.send(authors) // send list to browser
+    res.send(authors) // send list to browser
 }
-// Function to handle a request to a particular author
+
+// handle request to get one particular author
 const getAuthorByID = (req, res) => {
-    // search for author in the database via ID
-    const author = authors.find(author => author.id === req.params.id)
-    if (author){
-    res.send(author) // send back the author details
-    }
-    else{
-    // you can decide what to return if author is not found
-    // currently, an empty list will be returned
-    res.send([])
-    }
+	
+	// search for author by ID
+	const author = authors.find(author => author.id === req.params.id);
+	
+	if (author){
+		res.send(author); // send back the author details
+	}
+	else{
+		// you can decide what to return if author is not found
+		// currently, an empty list will be return.
+		res.send([]); 
+	}
 }
+
+// handle requests to add an author
+const addAuthor = (req, res) => {
+    // assemble a new author
+    newAuthor = req.body
+    // add to database
+    authors.push(newAuthor)
+    // return entire authors list to browser as a check that it worked
+    res.send(authors)
+}
+
+
 module.exports = {
- getAllAuthors,
- getAuthorByID
+    getAllAuthors, getAuthorByID, addAuthor
 }
